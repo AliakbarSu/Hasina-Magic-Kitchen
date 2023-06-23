f772e0a3a0c8632071b14a030ea1b1dfdcf6f72f
Ali Sultani <aliakbar.su@hotmail.com>
2023-06-23 17:24:29 +1200

    Initial commit

---
+++ resources/js/Pages/About.tsx
@@ -0,0 +1,12 @@
+import { Head } from '@inertiajs/react'
+
+export default function Welcome() {
+    return (
+        <div>
+
+            <h1>Welcome</h1>
+            <p>Hello Ali, welcome to your first Inertia app!</p>
+        </div>
+
+    )
+}
