38b175b2c257159ea9c8aab7a52771a75cae6f54
Ali Sultani <sultanialiasghar@gmail.com>
2023-07-05 20:46:28 +1200

    checkout functions updated

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,16 +4,29 @@ import '../css/app.css';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { store } from '@/store';
+import { Provider } from 'react-redux';
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const appName =
+    window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.tsx`,
+            import.meta.glob('./Pages/**/*.tsx')
+        ),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <>
+                <Provider store={store}>
+                    <App {...props} />
+                </Provider>
+            </>
+        );
     },
     progress: {
         color: '#4B5563',
