da627295c1d0383e6fe778b6ab81ea83254046be
Ali Sultani <aliakbar.su@hotmail.com>
2023-07-07 23:51:31 +1200

    Update appServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-        }
         Cashier::useCustomerModel(Customers::class);
     }
 }
