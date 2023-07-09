b69f9353d41f5e052af2bb5ef85520a3ee3dde4a
Ali Sultani <aliakbar.su@hotmail.com>
2023-07-09 15:24:12 +1200

    Fix database seeding issue

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ use App\Models\Orders;
 use Database\Factories\AdminFactory;
 use Faker\Factory;
 use Illuminate\Database\Seeder;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Stripe\Customer;
 
 class DatabaseSeeder extends Seeder
@@ -22,25 +24,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $path = Storage::path('test_files/default-photo1.jpg');
+        $file = new UploadedFile(
+            $path,
+            'default-photo1.jpg',
+            'image/jpg',
+            1234,
+            true
+        );
         AdminFactory::new()->create();
         $dishes = Dish::factory(4)->create();
-        $dishes->each(function ($dish) {
-            $dish->attachMedia();
+        $dishes->each(function ($dish) use ($file) {
+            $dish->attachMedia($file);
             return $dish;
         });
         Category::factory(3)->create();
-        $menus = Menu::factory(5)
+        $menus = Menu::factory(3)
             ->create()
-            ->each(function ($menu) use ($dishes) {
+            ->each(function ($menu) use ($dishes, $file) {
                 $menu->dishes()->attach($dishes->random(rand(1, 3)));
                 $menu->options()->attach($dishes->random(rand(1, 2)));
+                $menu->attachMedia($file);
+                return $menu;
             });
-
         $orders = Orders::factory(3)
             ->create()
             ->each(function ($order) use ($menus, $dishes) {
                 for ($i = 0; $i < 3; $i++) {
-                    $menu_id = $menus->toArray()[rand(0, 4)]['id'];
+                    $menu_id = $menus->toArray()[rand(0, 2)]['id'];
                     $itemOrder = new ItemsOrder();
                     $itemOrder->order_id = $order['id'];
                     $itemOrder->menu_id = $menu_id;
@@ -50,7 +61,7 @@ class DatabaseSeeder extends Seeder
                         $itemMenuOrder = new ItemsMenuOrder();
                         $itemMenuOrder->item_id = $itemOrder['id'];
                         $itemMenuOrder->dish_id = $dishes->toArray()[
-                            rand(0, 4)
+                            rand(0, 3)
                         ]['id'];
                         $itemMenuOrder->save();
                     }
